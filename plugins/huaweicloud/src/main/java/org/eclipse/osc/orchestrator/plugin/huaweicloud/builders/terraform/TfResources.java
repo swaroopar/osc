a564149fd43d0ca78f034a313925a273210cd727
swaroopar <swaroop.ar@outlook.com>
2023-02-02 07:04:56 +0100

    checkstyle added

--- plugins/huaweicloud/src/main/java/org/eclipse/osc/orchestrator/plugin/huaweicloud/builders/terraform/TfResources.java
+++ plugins/huaweicloud/src/main/java/org/eclipse/osc/orchestrator/plugin/huaweicloud/builders/terraform/TfResources.java
@@ -1,3 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ *
+ */
+
 package org.eclipse.osc.orchestrator.plugin.huaweicloud.builders.terraform;
 
 import java.util.ArrayList;
@@ -45,24 +51,3 @@ public class TfResources {
         return resources;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
